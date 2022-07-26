ce2df6935ff0ea05747bf652ac79bb1ae9017780
Okta Alfiansyah <oktaalfiansyah@gmail.com>
2022-07-26 14:30:27 +0700

    fix base_url for herokuapp

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/okta_alfian_portofolio/';
+$config['base_url'] = 'https://okta-alfian.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
